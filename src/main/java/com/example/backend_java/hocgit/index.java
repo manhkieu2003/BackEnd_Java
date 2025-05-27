5b56672b3b6e4532647b4c6e6bc014fd1d11dfb8
manhkd <manh12112@gmail.com>
2025-05-27 22:47:49 +0700

    commit2

--- src/main/java/com/example/backend_java/hocgit/index.java
+++ src/main/java/com/example/backend_java/hocgit/index.java
@@ -3,5 +3,6 @@ package com.example.backend_java.hocgit;
 public class index {
     public static void main(String[] args) {
         System.out.println("Hoc git index");
+        System.out.println("git thay doi modified");
     }
 }
